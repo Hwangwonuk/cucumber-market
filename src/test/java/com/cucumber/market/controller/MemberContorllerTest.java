7584ddfd9e0c8bd5a294721d9dc3121e4715f15a
Hwangwonuk <hok0173@naver.com>
2022-01-11 22:55:55 +0900

    회원 컨트롤러 회원조회 테스트코드 수정

--- src/test/java/com/cucumber/market/controller/MemberContorllerTest.java
+++ src/test/java/com/cucumber/market/controller/MemberContorllerTest.java
@@ -3,17 +3,22 @@ package com.cucumber.market.controller;
 import com.cucumber.market.dto.member.MemberInfo;
 import com.cucumber.market.resolver.CurrentMemberArgumentResolver;
 import com.cucumber.market.service.MemberService;
+import com.cucumber.market.service.SessionSignInService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(MemberController.class)
@@ -28,10 +33,13 @@ public class MemberContorllerTest {
     MemberService memberService;
 
     @MockBean
+    SessionSignInService sessionSignInService;
+
+    @MockBean
     CurrentMemberArgumentResolver currentMemberArgumentResolver;
 
     @Test
-    public void getMemberInfoTest() {
+    public void getMemberInfoTest() throws Exception {
         MemberInfo memberInfo = MemberInfo
                 .builder()
                 .member_id("tester")
@@ -42,7 +50,12 @@ public class MemberContorllerTest {
 
         when(memberService.getMemberInfo(any(String.class))).thenReturn(memberInfo);
 
-        //mockMvc.perform(get("/members")
-
+        mockMvc.perform(get("/members/myInfo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .param("member_id", "123")
+                        .param("isAdmin", "true"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
